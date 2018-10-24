1c3808c8a39aac9ec131ee8c8b63a92ea29bbbf5
atorresdi <atorresdi@gmail.com>
2018-10-24 14:31:39 +0200

    Test: synchronization between RTC and CPU clocks

--- main.c
+++ main.c
@@ -42,8 +42,27 @@ int main(void)
 
     while (1)
     {
-//        if (Tm_RTC_Period_Completed())
+        if (Tm_RTC_Period_Completed())
         {
+            // Test: Synchronization with RTC
+            uint32_t start_time, end_time, cycle_time;
+
+            // Wait for current RTC cycle to end
+            HWREG(AON_RTC_BASE + AON_RTC_O_SYNC) = 1; // write to force synchronization on read
+            while (HWREG(AON_RTC_BASE + AON_RTC_O_SYNC)) { }; // read to synchronize with next SCLK_LF edge
+
+            // Measure time of 1 RTC cycle
+            HWREG(AON_RTC_BASE + AON_RTC_O_SYNC) = 1; // write to force synchronization on read
+            start_time = Tm_Get_Free_Running_Timer_Val();
+            while (HWREG(AON_RTC_BASE + AON_RTC_O_SYNC)) { }; // read to synchronize with next SCLK_LF edge
+            end_time = Tm_Get_Free_Running_Timer_Val();
+
+            cycle_time = Tm_Delta_Time32(start_time, end_time);
+            PRINTF("cycle time: %lu ns\r\n", cycle_time * TM_HFXOSC_NSEC_PER_TICK);
+
+            GPIO_toggleDio(BRD_GREEN_LED);
+
+
 //            uint8_t payload[36];
 //            for (size_t n = 0; n < sizeof(payload); n++)
 //                payload[n] = n+1;
@@ -51,13 +70,13 @@ int main(void)
 //            Rad_Ble5_Adv_Aux(payload, sizeof(payload));
 //            GPIO_toggleDio(BRD_GREEN_LED);
 
-            uint8_t buf[256];
-            size_t payload_len;
-            payload_len = Rad_Ble5_Scanner(buf, sizeof(buf));
-            PRINTF("payload: ");
-            for (size_t n = 0; n < payload_len; n++)
-                PRINTF("%02x ", buf[n]);
-            PRINTF("\r\n");
+//            uint8_t buf[256];
+//            size_t payload_len;
+//            payload_len = Rad_Ble5_Scanner(buf, sizeof(buf));
+//            PRINTF("payload: ");
+//            for (size_t n = 0; n < payload_len; n++)
+//                PRINTF("%02x ", buf[n]);
+//            PRINTF("\r\n");
 
 //            uint32_t rat_delta = Tm_Delta_Time32(tx_result.rat_timestamp_start, tx_result.rat_timestamp_end);
 //            PRINTF("RTC: %lu, RAT start: %lu, RAT end: %lu, RAT delta: %lu\r\n",
