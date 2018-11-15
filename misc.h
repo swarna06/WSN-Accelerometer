b60355721cdb4c7e26e16cdc184627796ac3364c
Alvaro Torres Di Zeo <atorresdi@gmail.com>
2018-11-15 18:08:30 +0100

    Fixed small error in assertion macro

--- misc.h
+++ misc.h
@@ -20,19 +20,18 @@
 
 #if defined (ENABLE_ASSERT)
     #define assertion(__e)  if (__e) (void)0;   \
-                            else {               \
+                            else \
+                            { \
                                 PRINTF("Assertion violation: file " __FILE__ ", line " TO_STR(__LINE__) ": " #__e "\r\n"); \
-                                exit(0);}
+                                exit(0); \
+                            }
 #else
-    #define Assert(__e)
+    #define assertion(__e)
 #endif
 
 // Macro for checking if a pointer is memory aligned
 #define is_aligned(POINTER, BYTE_COUNT) \
         (((uintptr_t)(const void *)(POINTER)) % (BYTE_COUNT) == 0)
-
-
-
 
 
 #endif /* MISC_H_ */
