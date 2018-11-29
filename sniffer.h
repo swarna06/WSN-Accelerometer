12535fdc74a8deebd1c4d3ff0a2470bdb0333b03
Swarna Narayanan <s.narayanan@student.tudelft.nl>
2018-11-29 17:06:52 +0000

    sniffer.h for BLE sniffer module

---
+++ sniffer.h
@@ -0,0 +1,19 @@
+/*
+ * sniffer.h
+ *
+ *  Created on: 29 Nov 2018
+ *      Author: swarna
+ */
+
+#ifndef SNIFFER_H_
+#define SNIFFER_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <driverlib/rf_common_cmd.h>
+#include <inc/hw_rfc_rat.h>
+
+void BLE_sniffer(bool);
+
+#endif /* SNIFFER_H_ */
