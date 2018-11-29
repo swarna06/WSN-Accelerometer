869b9c49fc39f7075a388974ac8cb7adf34ae84e
Swarna Narayanan <s.narayanan@student.tudelft.nl>
2018-11-29 17:02:16 +0000

    main.c edited to include sniffer module

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 
 //#define DRIVERLIB_NOROM
-#define SET_CCFG_IEEE_BLE_0
 
 #include <driverlib/prcm.h>
 #include <driverlib/sys_ctrl.h>
@@ -29,27 +28,40 @@
 #include "log.h"
 #include "power_management.h"
 #include "profiling.h"
-
+#include "sniffer.h"
 #include "printf.h"
 
 PFL_DECLARE_PROFILING_VARIABLES();
 
 void GPIO_Init();
 
+extern rfc_control_t rfc;
+
 int main(void)
 {
     Pma_Init();
     GPIO_Init();
     Tm_Init();
-
     Sep_Init(); // FIXME disable to reduce power consumption
-    Log_Init();
-    #if (BRD_BOARD == BRD_LAUNCHPAD)
-    Pfl_Init(); // FIXME disable to reduce power consumption
-    #endif
-
     Rfc_Init();
-    Ptc_Init();
+    Log_Init();
+
+#if (BRD_BOARD == BRD_LAUNCHPAD)
+    Pfl_Init(); // FIXME disable to reduce power consumption
+#endif
+
+    int state = -1;
+     bool listen = false;
+    Rfc_Set_Tx_Power(RFC_TX_POW_0dBm);
+
+    Rfc_BLE5_Set_PHY_Mode(RFC_PHY_MODE_1MBPS);
+
+    Rfc_BLE5_Set_Channel(17); //0 to 39
+
+   //rfc_rx_result_t rx_result;
+    //uint8_t buf[256];
+    //rx_result.buf = buf;
+    //rx_result.buf_len = sizeof(buf);
 
     uint8_t count = 0;
     Brd_Led_Off(BRD_LED1);
@@ -68,8 +80,54 @@ int main(void)
         if (Tm_Period_Completed(TM_PER_HEARTBEAT_ID))
         {
             Brd_Led_Toggle(BRD_LED0);
-            Log_Val_Uint32("count: ", count++);
+            Log_Value("count: ", count++);
+            if(!listen && Rfc_Ready())
+            {
+                Rfc_BLE5_Scanner(800000); //1 sec
+                listen = true;
+            }
         }
+        //Transmit Packets
+/*
+        if (Tm_Period_Completed(TM_PER_HEARTBEAT_ID))
+        {
+            Brd_Led_Toggle(BRD_LED0);
+
+            uint8_t buf[16];
+            for (size_t n = 0; n < sizeof(buf); n++)
+                buf[n] = n;
+
+            rfc_tx_param_t tx_param;
+
+            tx_param.len = sizeof(buf);
+            tx_param.rat_start_time = 0;
+            tx_param.buf = buf;
+
+            Rfc_BLE5_Adv_Aux(&tx_param);
+        }
+
+
+*/
+
+        //Receive Packets
+   /*     if(rx_result.buf_len) (void) 0;
+
+        if(listen && Rfc_Ready())
+        {
+            listen = false;
+            Rfc_BLE5_Get_Scanner_Result(&rx_result);
+            if(!rx_result.err_flags)
+            {
+                for(int i=0; i< rx_result.payload_len; i++)
+                    Log_Value_Hex(buf[i]);
+                Log_Line("");
+
+            }
+            else Log_Line("no packet");
+
+        }*/
+
+        BLE_sniffer(listen);
 
         if (Tm_Sys_Tick())
             Tm_Process();
@@ -78,8 +136,12 @@ int main(void)
 
         Rfc_Process();
 
-        if (Rfc_Ready())
-            Ptc_Process();
+        if (state != rfc.state)
+        {
+            Log_Value("st ", (uint32_t)rfc.state);
+            state = rfc.state;
+        }
+
 
         continue;
 
@@ -143,7 +205,7 @@ int main(void)
         // ********************************
         Brd_Led_Off(BRD_LED0);
         Pma_MCU_Sleep(Tm_Get_RTC_Time() + WAKEUP_TIME_MS*TM_RTC_TICKS_PER_MSEC); // sleep
-//        Pma_MCU_Sleep(0); // sleep forever
+        //        Pma_MCU_Sleep(0); // sleep forever
     }
 
     return 0;
