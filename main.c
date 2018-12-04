7f983bc2d29879f109682bec8dca13168b98da1b
Alvaro Torres Di Zeo <atorresdi@gmail.com>
2018-12-04 22:24:09 +0100

    Cleanup: main

--- main.c
+++ main.c
@@ -53,15 +53,7 @@ int main(void)
     Rfc_Init();
     Ptc_Init();
 
-    uint8_t count = 0;
     Brd_Led_Off(BRD_LED1);
-
-    const uint32_t WAKEUP_TIME_MS = 100;
-    rfc_tx_param_t tx_param;
-    tx_param.buf = NULL; // empty packet
-    tx_param.rat_start_time = 0; // transmit immediately
-
-    uint32_t exec_time, wcet = 0;
 
     Tm_Start_Period(TM_PER_HEARTBEAT_ID, TM_PER_HEARTBEAT_VAL);
 
@@ -78,12 +70,9 @@ int main(void)
 
     while (1)
     {
-//        goto test;
-
         if (Tm_Period_Completed(TM_PER_HEARTBEAT_ID))
         {
 //            Brd_Led_Toggle(BRD_LED1);
-//            Log_Val_Uint32("count: ", count++);
         }
 
         if (Tm_Sys_Tick())
@@ -103,72 +92,6 @@ int main(void)
             Ptc_Process();
         else if (Rfc_Error())
             Ptc_Handle_Error();
-
-        continue;
-
-//        test:
-
-        // ********************************
-        // Active interval
-        // ********************************
-        Brd_Led_On(BRD_LED0);
-        AONRTCEventClear(AON_RTC_CH0);
-        AONRTCCompareValueSet(AON_RTC_CH0, Tm_Get_RTC_Time() + WAKEUP_TIME_MS*TM_RTC_TICKS_PER_MSEC);
-
-        // Wake up serial port
-        Sep_Wakeup();
-
-        // Wake up RF core
-        Pfl_Tic();
-
-        Rfc_Wakeup();
-        do
-        {
-            Rfc_Process();
-            Log_Process();
-            if (Tm_Sys_Tick())
-                Tm_Process();
-        } while (!Rfc_Ready());
-
-        Pfl_Toc();
-        exec_time = Pfl_Get_Exec_Time();
-        wcet = Pfl_Get_WCET();
-
-        PRINTF("count: %d\r\n", count++);
-        PRINTF("exec_time: %d ns, wcet: %d ns\r\n", Pfl_Ticks_To_Nanosec(exec_time), Pfl_Ticks_To_Nanosec(wcet));
-
-        // Transmit packet
-        Rfc_BLE5_Adv_Aux(&tx_param);
-        do
-        {
-            Rfc_Process();
-            Log_Process();
-            if (Tm_Sys_Tick())
-                Tm_Process();
-        } while (!Rfc_Ready());
-
-        // Blink LED using the timing module
-        Tm_Start_Timeout(TM_TOUT_TEST_ID, 90);
-        Tm_Start_Period(TM_PER_HEARTBEAT_ID, 30);
-        Brd_Led_On(BRD_LED1);
-        do
-        {
-            if (Tm_Sys_Tick())
-                Tm_Process();
-
-            if (Tm_Period_Completed(TM_PER_HEARTBEAT_ID))
-                Brd_Led_Toggle(BRD_LED1);
-        } while (!Tm_Timeout_Completed(TM_TOUT_TEST_ID));
-        Brd_Led_Off(BRD_LED1);
-
-        while (!AONRTCEventGet(AON_RTC_CH0)); // busy wait
-
-        // ********************************
-        // Sleep interval
-        // ********************************
-        Brd_Led_Off(BRD_LED0);
-        Pma_MCU_Sleep(Tm_Get_RTC_Time() + WAKEUP_TIME_MS*TM_RTC_TICKS_PER_MSEC); // sleep
-//        Pma_MCU_Sleep(0); // sleep forever
     }
 
     return 0;
