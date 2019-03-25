0d708a7f22e58d21098b500e6b0c5042da343789
Alvaro Torres Di Zeo <atorresdi@gmail.com>
2019-03-25 10:36:41 +0100

    Added drift compensation

--- sync_test.c
+++ sync_test.c
@@ -22,6 +22,10 @@
 
 bool proceed = false;
 
+static uint8_t subsec_inc_offset[] = {0, 8, 17, 25, 34, 42, 50,
+                                      59, 67, 75, 84, 92, 101, 109,
+                                      117, 126, 134, 143, 151, 159, 168};
+
 static sts_control stc;
 
 static void Sts_Sink_Process();
@@ -30,6 +34,7 @@ static void Sts_Sensor_Process();
 static uint32_t Sts_Get_RTC_Time();
 static bool Sts_Get_RAT_Int_Flag();
 static void Sts_Clear_RAT_Int_Flag();
+static void Sts_Compensate_Drift(int32_t offset_ticks);
 
 #ifdef CFG_DEBUG_START_OF_FRAME_OUT
 void Sts_RTC_Isr();
@@ -74,6 +79,10 @@ void Sts_Init()
     stc.rx_param.dest_buf_len = RAD_MAX_PAYLOAD_LEN;
     stc.rx_param.start_time = 0;
     stc.rx_param.timeout_usec = 0;
+
+    stc.samp_idx = 0;
+    for (size_t i = 0; i < STS_SAMP_NUM; i++)
+        stc.sync_err_samples[i] = 0;
 
     #ifdef CFG_DEBUG_START_OF_FRAME_OUT
     // Configure an RTC CH in compare mode and enable interrupt
@@ -354,9 +363,21 @@ static void Sts_Sensor_Process()
                 stc.rtc_sync_time = (rtc_curr_time - rtc_delta) + STS_SYNC_PERIOD_RTC;
                 stc.rtc_wakeup_time = stc.rtc_sync_time - STS_WAKEUP_DELAY;
 
-                int32_t sync_err = stc.rx_param.timestamp - stc.rat_sync_time;
-                Log_Val_Int32("sync_err:", sync_err);
+                int32_t sync_err_ticks = stc.rx_param.timestamp - stc.rat_sync_time;
+//                Log_Val_Int32("sync_err:", sync_err_ticks);
                 stc.rat_sync_time = stc.rx_param.timestamp + STS_SYNC_PERIOD_RAT;
+
+                Sts_Compensate_Drift(sync_err_ticks);
+
+//                // Adjust offset
+//                uint32_t new_rate = 8388583; // -3 ppm
+////                uint32_t new_rate = 8388633; // +3 ppm
+//                uint16_t new_rate_0 = 0x0000FFFF & new_rate;
+//                uint16_t new_rate_1 = (0x00FF0000 & new_rate) >> 16;
+//
+//                HWREG(AUX_WUC_BASE + AUX_WUC_O_RTCSUBSECINC0) = new_rate_0;
+//                HWREG(AUX_WUC_BASE + AUX_WUC_O_RTCSUBSECINC1) = new_rate_1;
+//                HWREG(AUX_WUC_BASE + AUX_WUC_O_RTCSUBSECINCCTL) = 0x01;
             }
             else
             {
@@ -421,6 +442,56 @@ static inline bool Sts_Get_RAT_Int_Flag()
 static inline void Sts_Clear_RAT_Int_Flag()
 {
     HWREG(RFC_DBELL_BASE + RFC_DBELL_O_RFHWIFG) = ~RFC_DBELL_RFHWIFG_RATCH5;
+}
+
+static void Sts_Compensate_Drift(int32_t offset_ticks)
+{
+    // Add new sample, update index and calculate average
+    stc.sync_err_samples[stc.samp_idx] = offset_ticks;
+    if (stc.samp_idx < STS_SAMP_NUM - 1)
+        stc.samp_idx++;
+    else
+        stc.samp_idx = 0;
+
+    int32_t sum = 0;
+    for (size_t i = 0; i < STS_SAMP_NUM; i++)
+        sum += stc.sync_err_samples[i];
+
+    Log_Val_Int32("sum:", sum);
+    bool negative = false;
+    if (sum < 0)
+    {
+        sum = sum * (-1); // this has to be done to avoid error in the rounding (modulo & negative)
+        negative = true;
+    }
+
+    uint32_t average = sum / STS_SAMP_NUM;
+    if (sum % STS_SAMP_NUM > STS_SAMP_NUM/2) // round to nearest
+        average++;
+
+    // Calculate ppm offset and adjust clock rate
+    uint32_t ppm = average/(RAD_RAT_TICKS_PER_USEC * STS_SYNC_PER_SEC);
+    assertion(ppm < sizeof(subsec_inc_offset));
+
+    uint32_t new_rate = HWREG(AON_RTC_BASE + AON_RTC_O_SUBSECINC);
+    if (negative == false)
+        new_rate -= subsec_inc_offset[ppm];
+    else
+        new_rate += subsec_inc_offset[ppm];
+
+    uint16_t new_rate_0 = 0x0000FFFF & new_rate;
+    uint16_t new_rate_1 = (0x00FF0000 & new_rate) >> 16;
+
+    HWREG(AUX_WUC_BASE + AUX_WUC_O_RTCSUBSECINC0) = new_rate_0;
+    HWREG(AUX_WUC_BASE + AUX_WUC_O_RTCSUBSECINC1) = new_rate_1;
+    HWREG(AUX_WUC_BASE + AUX_WUC_O_RTCSUBSECINCCTL) = 0x01;
+
+    Log_Val_Int32("new:", offset_ticks);
+    Log_Val_Int32("ave:", average);
+
+    Log_Val_Int32("ppm:", ppm);
+    Log_Val_Int32("offs:", subsec_inc_offset[ppm]);
+    Log_Val_Int32("rate:", new_rate);
 }
 
 
--- sync_test.h
+++ sync_test.h
@@ -12,14 +12,17 @@
 
 #include "radio.h"
 
-#define STS_SYNC_PER_DIV        16
+#define STS_SYNC_PER_SEC        1
+#define STS_SYNC_PER_DIV        1
 
-#define STS_SYNC_PERIOD_RTC     ((1<<16)/STS_SYNC_PER_DIV) // 1 second
+#define STS_SYNC_PERIOD_RTC     (((1<<16)/STS_SYNC_PER_DIV)*STS_SYNC_PER_SEC) // 1 second
 #define STS_WAKEUP_DELAY        (TM_RTC_TICKS_PER_MSEC * 2) // 2 milliseconds
 
-#define STS_SYNC_PERIOD_RAT     ((4000000)/STS_SYNC_PER_DIV) // 1 second
+#define STS_SYNC_PERIOD_RAT     (((4000000)/STS_SYNC_PER_DIV)*STS_SYNC_PER_SEC) // 1 second
 
 #define STS_RADIO_OP_DELAY      (762 + 5) // 191.4 usec / 0.25 usec (+ 5 trimming)
+
+#define STS_SAMP_NUM            4
 
 enum
 {
@@ -61,6 +64,9 @@ typedef struct
     rad_tx_param_t tx_param;
     rad_rx_param_t rx_param;
     uint8_t txrx_buf[RAD_MAX_PAYLOAD_LEN];
+
+    int32_t sync_err_samples[STS_SAMP_NUM];
+    size_t samp_idx;
 
     uint8_t flags;
     uint8_t state;
